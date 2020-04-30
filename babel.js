6fe8b33a8ce7cfe789f48aa99f1a5c61283d65a1
Jason Miller <developit@users.noreply.github.com>
2020-04-30 15:16:04 -0400

    Stateless Babel plugin & support/detect multiple components

--- babel.js
+++ babel.js
@@ -1,34 +1,51 @@
 const property = '__refresh_constructor__';
 
-const plugin = ({ template, types: t }) => {
-  let inConstructor = false;
-  let _constructor = undefined;
-
+function extractComponentConstructors ({ template, types: t }) => {
   return {
+    name: 'transform-extract-component-constructors',
     visitor: {
-      ClassBody(path) {
-        _constructor = t.classMethod("method", t.identifier(property), [], t.blockStatement([]));
-        path.node.body.push(_constructor);
-      },
       ClassMethod(path) {
-        if (path.node.kind === "constructor") {
-          inConstructor = true;
-          path.node.body.body.push(
-            t.expressionStatement(t.callExpression(t.memberExpression(t.thisExpression(), t.identifier(property)), []))
-          );
-        } else {
-          inConstructor = false;
-        }
-      },
-      ExpressionStatement(path) {
-        const { node } = path;
-        if (_constructor && inConstructor && node.expression.right && node.expression.right.callee && node.expression.right.callee.property.name === "bind") {
-          _constructor.body.body.push(node);
-          path.remove();
-        }
+        if (path.node.kind !== "constructor") return;
+        
+        const classBody = path.parent.body;
+        const hasRenderMethod = classBody.some(
+          p => t.isClassMethod(p) && t.isIdentifier(p.key, {name:'render'})
+        );
+        // no render() method, not a Component.
+        if (!hasRenderMethod) return;
+
+        // extract everything *except* the super() call
+        const body = path.get('body.body').reduce((body, p) => {
+          let expr = t.isExpressionStatement(p) ? p.get('expression') : p;
+          if (!t.isCallExpression(expr) || !t.isSuper(expr.get('callee'))) {
+            body.push(t.clone(p.node));
+            p.remove();
+          }
+          return body;
+        }, []);
+        
+        path.get('body').pushContainer(
+          'body',
+          t.callExpression(
+            t.memberExpression(
+              t.thisExpression(),
+              t.identifier(property)
+            ),
+            path.node.params.map(t.clone)
+          )
+        );
+        
+        path.insertAfter(
+          t.classMethod(
+            'method',
+            t.identifier(property),
+            path.node.params.map(t.clone),
+            t.blockStatement(body)
+          )
+        );
       }
     }
   };
 };
 
-module.exports = plugin;
+module.exports = extractComponentConstructors;
