bedd3a51f2c99b2a3c600ab405a6849686877712
Jovi De Croock <decroockjovi@gmail.com>
2020-05-03 10:57:09 +0200

    retaining hooks state (#5)
    * func component fix
    
    * retaining hooks state
    
    * remove unneeded stuff
    
    * add check for lacking hot reload id
    
    * remove id assertion
    
    * add comment

--- src/runtime.js
+++ src/runtime.js
@@ -3,33 +3,25 @@ import { options, Component } from 'preact';
 // all vnodes referencing a given constructor
 const vnodesForComponent = new WeakMap();
 
-function replaceComponent(oldType, newType) {
-	const vnodes = vnodesForComponent.get(oldType);
+function replaceComponent(OldType, NewType) {
+	const vnodes = vnodesForComponent.get(OldType);
 	if (!vnodes) return;
 
 	// migrate the list to our new constructor reference
-	vnodesForComponent.delete(oldType);
-	vnodesForComponent.set(newType, vnodes);
+	vnodesForComponent.delete(OldType);
+	vnodesForComponent.set(NewType, vnodes);
 
 	vnodes.forEach(vnode => {
 		// update the type in-place to reference the new component
-		vnode.type = newType;
+		vnode.type = NewType;
 
 		if (vnode.__c) {
 			vnode.__c.constructor = vnode.type;
-			if (vnode.__c.__H) {
-				// Reset hooks state
-				vnode.__c.__H = {
-					__: [], // _list
-					__h: [] // _pendingEffects
-				};
-			}
 
 			try {
-				// if this was/is a class component:
-				if (vnode.__c instanceof oldType) {
+				if (vnode.__c instanceof OldType) {
 					const oldInst = vnode.__c;
-					const newInst = new newType(vnode.__c.props, vnode.__c.context);
+					const newInst = new NewType(vnode.__c.props, vnode.__c.context);
 					vnode.__c = newInst;
 					// copy old properties onto the new instance.
 					//   - Objects (including refs) in the new instance are updated with their old values
@@ -57,7 +49,35 @@ function replaceComponent(oldType, newType) {
 				/* Functional component */
 			}
 
-			Component.prototype.forceUpdate.call(vnode.__c);
+			if (vnode.__c.__H) {
+				const visited = new WeakSet();
+				const hooks = vnode.__c.__H;
+				const oldOptionsHook = options.__h;
+				options.__h = (component, index, type) => {
+					const hooks = component.__H.__;
+					if (hooks[index].type !== type) {
+						hooks.splice(index, 0, { type });
+					} else if (hooks[index].__hot_reload_id__) {
+						visited.add(hooks[index].__hot_reload_id__);
+					}
+
+					if (oldOptionsHook) oldOptionsHook(component, index, type);
+				};
+
+				Component.prototype.forceUpdate.call(vnode.__c, () => {
+					// We have finished the update, all that's left to do is check whether
+					// or not we have unvisited hooks, if we do we can safely remove them since
+					// this code could've been removed.
+					vnode.__c.__H.__.forEach(({ __hot_reload_id__: hotReloadId }, i) => {
+						if (hotReloadId && !visited.has(hotReloadId)) {
+							hooks.__.splice(i, 1);
+						}
+					});
+					options.__h = oldOptionsHook;
+				});
+			} else {
+				Component.prototype.forceUpdate.call(vnode.__c);
+			}
 		}
 	});
 }
@@ -88,6 +108,16 @@ options.__b = (oldVNode, newVNode) => {
 			vnodes.splice(index, 1);
 		}
 	}
+
+	if (typeof type === 'function' && newVNode.__c && newVNode.__c.__H) {
+		const hooks = newVNode.__c.__H;
+		(hooks.__ || []).forEach(listItem => {
+			if (!listItem.__hot_reload_id__) {
+				listItem.__hot_reload_id__ = Symbol('__hot_reload_id__');
+			}
+		});
+	}
+
 	if (oldDiffed) oldDiffed(oldVNode, newVNode);
 };
 
@@ -102,4 +132,20 @@ options.unmount = vnode => {
 		}
 	}
 	if (oldUnmount) oldUnmount(vnode);
+};
+
+const oldHook = options.__h;
+options.__h = (comp, index, type) => {
+	if (!comp.__H) {
+		comp.__H = {
+			__: [], // _list
+			__h: [] // _pendingEffects
+		};
+	}
+
+	if (!comp.__H.__[index]) {
+		comp.__H.__.push({ type });
+	}
+
+	if (oldHook) oldHook(comp, index, type);
 };
--- src/utils.js
+++ src/utils.js
@@ -1,24 +1,23 @@
 const injectEntry = originalEntry => {
-  const entryInjects = [require.resolve('./runtime.js'),];
+	const entryInjects = [require.resolve('./runtime.js')];
 
-  if (typeof originalEntry === 'string') {
-    return [...entryInjects, originalEntry];
-  }
+	if (typeof originalEntry === 'string') {
+		return [...entryInjects, originalEntry];
+	}
 
-  if (Array.isArray(originalEntry)) {
-    return [...entryInjects, ...originalEntry];
-  }
+	if (Array.isArray(originalEntry)) {
+		return [...entryInjects, ...originalEntry];
+	}
 
-  if (typeof originalEntry === 'object') {
-    return Object.entries(originalEntry).reduce(
-      (acc, [curKey, curEntry]) => ({
-        ...acc,
-        [curKey]: injectEntry(curEntry),
-      }),
-      {}
-    );
-  }
+	if (typeof originalEntry === 'object') {
+		return Object.entries(originalEntry).reduce(
+			(acc, [curKey, curEntry]) => ({
+				...acc,
+				[curKey]: injectEntry(curEntry)
+			}),
+			{}
+		);
+	}
 };
 
-exports.countStatefulHooks = (hooks) => hooks.reduce((acc, hook) => Array.isArray(hook.__) ? acc + 1 :  acc, 0)
 exports.injectEntry = injectEntry;
