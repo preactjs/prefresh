05fa9fae5930cc433a090248bee3d55b709d25d8
jdecroock <decroockjovi@gmail.com>
2020-05-03 02:51:14 +0200

    remove unneeded stuff

--- src/utils.js
+++ src/utils.js
@@ -20,15 +20,4 @@ const injectEntry = originalEntry => {
 	}
 };
 
-exports.countStatefulHooks = hooks =>
-	hooks.reduce((acc, hook) => (Array.isArray(hook.__) ? acc + 1 : acc), 0);
 exports.injectEntry = injectEntry;
-
-const isMemoHook = hook => !!hook.__h;
-
-exports.isStateFulHook = hook => Array.isArray(hook.__); // HookState._value is an array of value and dispatch
-exports.isEffectHook = hook => typeof hook.__ === 'function'; // HookState._value is a function (will also be useImperativeHandle)
-exports.isRefHook = hook =>
-	isMemoHook(hook) && typeof hook.__ === 'object' && hook.__.current;
-exports.isMemoHook = isMemoHook;
-exports.isContextHook = hook => !!hook.__c;
