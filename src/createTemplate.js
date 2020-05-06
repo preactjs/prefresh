c8c1d8e2a60fb24e0d1fa683a4bf2a32bdcf0fee
Jovi De Croock <decroockjovi@gmail.com>
2020-05-06 22:49:24 +0200

    only bind refresh to components (#8)
    * only bind refresh to components
    
    * ensure only binding preact components
    
    * Update utils.js
    
    * Apply suggestions from code review
    
    Co-authored-by: Jason Miller <developit@users.noreply.github.com>
    
    Co-authored-by: Jason Miller <developit@users.noreply.github.com>

--- src/createTemplate.js
+++ src/createTemplate.js
@@ -2,15 +2,37 @@ const { Template } = require('webpack');
 const { NAMESPACE } = require('./constants');
 
 const afterModule = `
-if (module.hot) {
+const exports = module.exports || module.__proto__.exports;
+let shouldBind = false;
+
+if (!exports || typeof exports != 'object') {
+  shouldBind = false;
+} else {
+  for (const key in exports) {
+    if (key === '__esmodule') continue;
+    const exportValue = exports[key];
+    if (typeof exportValue == 'function') {
+      const name = exportValue.name || exportValue.displayName;
+      if (name) {
+        shouldBind = shouldBind || (typeof name === 'string' && name[0] == name[0].toUpperCase());
+      }
+    }
+  }
+}
+
+if (module.hot && shouldBind) {
   const m = module.hot.data && module.hot.data.module && module.hot.data.module;
   if (m) {
     for (let i in module.exports) {
       const fn = module.exports[i];
-      if (typeof fn === 'function') {
-        if (i in m.exports) {
-          window.${NAMESPACE}.replaceComponent(m.exports[i], fn);
+      try {
+        if (typeof fn === 'function') {
+          if (i in m.exports) {
+            self.${NAMESPACE}.replaceComponent(m.exports[i], fn);
+          }
         }
+      } catch (e) {
+        self.location.reload();
       }
     }
   }
