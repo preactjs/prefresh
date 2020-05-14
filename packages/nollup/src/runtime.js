38827fef81ae5ea80df48c4e09dc12d7cd735987
Paul Sweeney <paul-sweeney@outlook.ie>
2020-05-14 12:44:51 +0100

    Nollup Integration (#27)
    Co-authored-by: Sweeney <paul.p.sweeney@accenture.com>

---
+++ packages/nollup/src/runtime.js
@@ -0,0 +1,69 @@
+import { isComponent, compareSignatures } from '@prefresh/utils';
+
+const getExports = m => m.exports || m.__proto__.exports;
+
+const registerExports = m => {
+	let isCitizen = false;
+	const moduleId = module.id;
+	const moduleExports = getExports(m);
+
+	if (typeof moduleExports === 'function' && isComponent(moduleExports.name)) {
+		isCitizen = true;
+		window.__PREFRESH__.register(moduleExports, moduleId + ' %exports%');
+	}
+
+	if (
+		moduleExports === undefined ||
+		moduleExports === null ||
+		typeof moduleExports !== 'object'
+	) {
+		isCitizen = isCitizen || false;
+	} else {
+		for (const key in moduleExports) {
+			if (key === '__esModule') continue;
+
+			const exportValue = moduleExports[key];
+			if (typeof exportValue === 'function' && isComponent(exportValue.name)) {
+				isCitizen = isCitizen || true;
+				const typeID = moduleId + ' %exports% ' + key;
+				window.__PREFRESH__.register(exportValue, typeID);
+			}
+		}
+	}
+
+	return isCitizen;
+};
+
+export function __$RefreshCheck$__(module) {
+	const isCitizen = registerExports(module);
+
+	if (module.hot && isCitizen) {
+		const moduleExports = getExports(module);
+		const m =
+			module.hot.data && module.hot.data.module && module.hot.data.module;
+
+		if (m) {
+			for (let i in moduleExports) {
+				const fn = moduleExports[i];
+				try {
+					if (typeof fn === 'function') {
+						const oldExports = getExports(m);
+						if (i in oldExports) {
+							compareSignatures(oldExports[i], fn);
+						}
+					}
+				} catch (e) {
+					self.location.reload();
+				}
+			}
+		}
+
+		module.hot.dispose(function(data) {
+			data.module = module;
+		});
+
+		module.hot.accept(function() {
+			require(module.id);
+		});
+	}
+}
