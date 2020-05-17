9866906a3385c02593aaa7cd6092d047bb135574
Jovi De Croock <decroockjovi@gmail.com>
2020-05-18 00:45:13 +0200

    add babel transform support

--- packages/snowpack/src/index.js
+++ packages/snowpack/src/index.js
@@ -1,35 +1,56 @@
-import { isComponent } from '@prefresh/utils';
+import { compareSignatures /*, isComponent*/ } from '@prefresh/utils';
 
 export default function preactRefreshPlugin(config, pluginOptions) {
-  return {
-    knownEntrypoints: ['@prefresh/core'],
-    async transform({ contents, urlPath, isDev }) {
-      if (!isDev || !urlPath.endsWith('.js')) return;
+	return {
+		knownEntrypoints: ['@prefresh/core'],
+		async transform({ contents, urlPath, isDev }) {
+			if (!isDev || !urlPath.endsWith('.js')) return;
 
-      // TODO: check if we can use `isComponent` on $CurrentModule$
-      // so we can skip binding custom-hooks. Hooks won't be supported until the acorn
-      // plugi neither way.
-      return {
-        result: `
+			// TODO: only bind components with isComponent
+			return {
+				result: `
           import '@prefresh/core';
           import * as $OriginalModule$ from ${JSON.stringify(urlPath)};
           let $CurrentModule$ = $OriginalModule$;
 
+          const compareSignaturesForPrefreshment = ${compareSignatures.toString()};
+
+          window.$RefreshSig$ = () => {
+            let status = 'begin';
+            let savedType;
+            return (type, key, forceReset, getCustomHooks) => {
+              if (!savedType) savedType = type;
+              status = self.__PREFRESH__.sign(type || savedType, key, forceReset, getCustomHooks, status);
+            };
+          };
+
+          window.$RefreshReg$ = (type, id) => {
+            self.__PREFRESH__.register(type, module.i + ' ' + id);
+          };
+
           ${contents}
 
+          conosle.log($CurrentModule$, import)
           if (import.meta.hot) {
-            import.meta.hot.accept(({module}) => {
+            import.meta.hot.accept(({ module }) => {
               try {
                 for (let i in module) {
-                  self.__PREFRESH__.replaceComponent($CurrentModule$[i], module[i]);
+                  if (typeof module[i] === 'function') {
+                    if (i in $CurrentModule$) {
+                      compareSignaturesForPrefreshment(
+                        $CurrentModule$[i],
+                        module[i]
+                      );
+                    }
+                  }
                 }
                 $CurrentModule$ = module;
               } catch(e) {
                 window.location.reload();
               }
             });
-          }`,
-      };
-    },
-  };
-};
+          }`
+			};
+		}
+	};
+}
