1ed1d9de17d579bb2e364edbbaada56d0ce06edb
Jovi De Croock <decroockjovi@gmail.com>
2020-05-17 18:52:13 +0200

    add snowpack package

---
+++ packages/snowpack/README.md
@@ -0,0 +1,21 @@
+# `@prefresh/snowpack`
+
+**Experimental package**
+
+[![npm version](https://badgen.net/npm/v/@prefresh/snowpack)](https://www.npmjs.com/package/@prefresh/snowpack)
+
+## Setup
+
+```
+npm i -s @prefresh/snowpack
+## OR
+yarn add @prefresh/snowpack
+```
+
+Then add it to your `snowpack.config.json` config:
+
+```json
+{
+  "plugins": ["@prefresh/snowpacks"]
+}
+```
---
+++ packages/snowpack/src/index.js
@@ -0,0 +1,31 @@
+import { isComponent } from '@prefresh/utils';
+
+export default function preactRefreshPlugin(config, pluginOptions) {
+  return {
+    knownEntrypoints: ['@prefresh/core'],
+    async transform({ contents, urlPath, isDev }) {
+      if (!isDev || !urlPath.endsWith('.js')) return;
+
+      // TODO: check if we can use `isComponent` on $CurrentModule$
+      // so we can skip binding custom-hooks. Hooks won't be supported until the acorn
+      // plugi neither way.
+      return {
+        result: `
+          import '@prefresh/core';
+          import * as $OriginalModule$ from ${JSON.stringify(urlPath)};
+          let $CurrentModule$ = $OriginalModule$;
+
+          ${contents}
+
+          if (import.meta.hot) {
+            import.meta.hot.accept(({module}) => {
+              for (let i in module) {
+                self.__PREFRESH__.replaceComponent($CurrentModule$[i], module[i]);
+              }
+              $CurrentModule$ = module;
+            });
+          }`,
+      };
+    },
+  };
+};
