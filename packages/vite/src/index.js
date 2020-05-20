d4101566f01dcc587b9926f73f61acf99d9d14bb
Jovi De Croock <decroockjovi@gmail.com>
2020-05-20 19:10:19 +0200

    finalize api

--- packages/vite/src/index.js
+++ packages/vite/src/index.js
@@ -1,30 +1,11 @@
-const preparation = `
-const prevRefreshReg = window.$RefreshReg$ || (() => {});
-const prevRefreshSig = window.$RefreshSig$ || (() => {});
-`;
-
-const signatureRegistration = `
-window.$RefreshSig$ = () => {
-  let status = 'begin';
-  let savedType;
-  return (type, key, forceReset, getCustomHooks) => {
-    if (!savedType) savedType = type;
-    status = self.__PREFRESH__.sign(type || savedType, key, forceReset, getCustomHooks, status);
-  };
-};
-`;
-
-const cleanup = `
-window.$RefreshReg$ = prevRefreshReg;
-window.$RefreshSig$ = prevRefreshSig;
-`;
+import { transformSync } from '@babel/core';
 
 /** @returns {import('vite').Plugin} */
 export default function prefreshPlugin() {
 	return {
 		transforms: [
 			{
-				test: path => /\.(t|j)s$/.test(path),
+				test: path => /\.(t|j)s(x)?$/.test(path),
 				transform(code, _, isBuild, path) {
 					if (
 						isBuild ||
@@ -33,76 +14,58 @@ export default function prefreshPlugin() {
 					)
 						return code;
 
-					const result = require('@babel/core').transformSync(code, {
-						plugins: [require('react-refresh/babel')],
-						ast: false,
-						sourceMaps: false
-					});
+					const result = transform(code);
+					const shouldBind = result.code.includes('$RefreshReg$(');
 
 					return `
-            ${preparation}
+            ${
+							shouldBind
+								? `
+              import '@prefresh/core';
+              import { compareSignatures } from '@prefresh/utils';
+              import { hot } from 'vite/hmr';
+            `
+								: ''
+						}
 
-            window.$RefreshReg$ = (type, id) => {}
-
-            ${signatureRegistration}
-
-            ${result.code}
-
-            if (__DEV__) {
-              ${cleanup}
-            }
-          `;
-				}
-			},
-			{
-				test: path => /\.(t|j)sx$/.test(path),
-				transform(code, _, isBuild, path) {
-					if (
-						isBuild ||
-						process.env.NODE_ENV === 'production' ||
-						path.includes('@modules')
-					)
-						return code;
-
-					const spec = JSON.stringify(path);
-
-					const result = require('@babel/core').transformSync(code, {
-						plugins: [require('react-refresh/babel')],
-						ast: false,
-						sourceMaps: false
-					});
-
-					return `
-            import '@prefresh/core';
-            import { compareSignatures, isComponent } from '@prefresh/utils';
-            import { hot } from 'vite/hmr';
-            import * as __PSELF__ from ${spec};
-
-            ${preparation}
+            const prevRefreshReg = window.$RefreshReg$ || (() => {});
+            const prevRefreshSig = window.$RefreshSig$ || (() => {});
 
             const module = {};
-            let hasComponents = false;
 
             window.$RefreshReg$ = (type, id) => {
               module[type.name] = type;
-              if (isComponent(type.name)) hasComponents = true;
             }
 
-            ${signatureRegistration}
+            window.$RefreshSig$ = () => {
+              let status = 'begin';
+              let savedType;
+              return (type, key, forceReset, getCustomHooks) => {
+                if (!savedType) savedType = type;
+                status = self.__PREFRESH__.sign(type || savedType, key, forceReset, getCustomHooks, status);
+              };
+            };
 
             ${result.code}
 
             if (__DEV__) {
-              ${cleanup}
-              hot.accept((m) => {
-                try {
-                  for (let i in m) {
-                    compareSignatures(module[i], m[i]);
+              window.$RefreshReg$ = prevRefreshReg;
+              window.$RefreshSig$ = prevRefreshSig;
+              ${
+								shouldBind
+									? `
+                hot.accept((m) => {
+                  try {
+                    for (let i in m) {
+                      compareSignatures(module[i], m[i]);
+                    }
+                  } catch (e) {
+                    window.location.reload();
                   }
-                } catch (e) {
-                  window.location.reload();
-                }
-              });
+                });
+              `
+									: ''
+							}
             }
           `;
 				}
@@ -110,3 +73,10 @@ export default function prefreshPlugin() {
 		]
 	};
 }
+
+const transform = code =>
+	transformSync(code, {
+		plugins: [require('react-refresh/babel')],
+		ast: false,
+		sourceMaps: false
+	});
