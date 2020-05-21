247d279c4ba4820319618f40f4a5e0b8b2a90f0d
Jovi De Croock <decroockjovi@gmail.com>
2020-05-22 00:58:38 +0200

    add snowpack example and improve comp check (#48)

---
+++ packages/snowpack/example/snowpack.config.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const cwd = process.cwd();
+const isTS = fs.existsSync(path.join(cwd, 'tsconfig.json'));
+
+const scripts = {
+  'mount:public': 'mount public --to /',
+  'mount:web_modules': 'mount web_modules',
+  'mount:src': 'mount src --to /_dist_',
+};
+
+if (isTS) {
+  scripts['run:tsc'] = 'tsc --noEmit';
+  scripts['run:tsc::watch'] = '$1 --watch';
+}
+
+module.exports = {
+  scripts,
+  devOptions: {},
+  installOptions: {
+    installTypes: isTS,
+  },
+  plugins: ['@snowpack/plugin-babel', '..'],
+};
