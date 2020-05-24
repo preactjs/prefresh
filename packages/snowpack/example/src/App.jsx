d8f2f3d1f5bd91ff50f99dd7ebe4cfef12bb7103
jdecroock <decroockjovi@gmail.com>
2020-05-24 16:00:44 +0200

    minor example correction

--- packages/snowpack/example/src/App.jsx
+++ packages/snowpack/example/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [count, increment, decrement] = useCounter()
   return (
     <div style={{ display: 'flex' }}>
-      <button onClick={decrement}>-</button>
+      <button onClick={decrement}>-10</button>
       <p>Count: {count}</p>
       <button onClick={increment}>+10</button>
     </div>
