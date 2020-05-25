ea07e47c5767e083ab80148203e578afc8032b23
jdecroock <decroockjovi@gmail.com>
2020-05-25 12:07:33 +0200

    update example

--- packages/snowpack/example/src/useCounter.js
+++ packages/snowpack/example/src/useCounter.js
@@ -2,5 +2,5 @@ import { useState } from 'preact/hooks';
 
 export const useCounter = () => {
   const [count, setCount] = useState(0);
-  return [count, () => setCount(count + 10), () => setCount(count - 10)];
+  return [count, () => setCount(count + 20), () => setCount(count - 20)];
 };
