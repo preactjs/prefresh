16b7ea9bd972fe7bdf12bbe17a7d8d65d317f0c0
Jovi De Croock <decroockjovi@gmail.com>
2020-05-31 15:22:52 +0200

    start on runtime warnings

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,3 +1,5 @@
+const warnings = new Set();
+
 export const compareSignatures = (prev, next) => {
 	const prevSignature = self.__PREFRESH__.getSignature(prev) || {};
 	const nextSignature = self.__PREFRESH__.getSignature(next) || {};
@@ -29,3 +31,42 @@ export const isCustomHook = name =>
 
 export const isComponent = name =>
 	typeof name === 'string' && name[0] && name[0] == name[0].toUpperCase();
+
+export const scanExports = (moduleExports, id) => {
+	let hasExportedHook = false;
+	let hasExportedComponent = false;
+
+	const exportedHooks = [];
+	const exportedComponents = [];
+
+	moduleExports.forEach(exportName => {
+		// TODO: warn when a component/hook has no name.
+		// For instance: export default () => <p>hi</p>
+		if (isComponent(exportName)) {
+			exportedComponents.push(exportName);
+			hasExportedComponent = true;
+		} else if (isCustomHook(exportName)) {
+			exportedHooks.push(exportName);
+			hasExportedHook = true;
+		}
+	});
+
+	if (warnings.has(id)) {
+		if (!hasExportedComponent || !hasExportedHook) warnings.delete(id);
+		return;
+	}
+
+	if (hasExportedComponent && hasExportedHook) {
+		warnings.add(id);
+		console.warn(`
+      [Prefresh]
+
+      You are combining hook and component exports in the same module, this could lead to unexpected behavior in prefresh.
+
+      Exported components: ${exportedComponents.join(', ')}.
+      Exported hooks: ${exportedHooks.join(', ')}.
+
+      It's advised to move the hooks to a seperate file.
+    `);
+	}
+};
