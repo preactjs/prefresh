77db90d4846451ad89b45ea9b5d35ab246551f8e
Marvin Hagemeister <hello@marvinh.dev>
2020-08-11 14:47:51 +0200

    Add hello world vite test

--- packages/vite/test/health.test.mjs
+++ packages/vite/test/health.test.mjs
@@ -2,7 +2,7 @@ import expect from 'expect';
 import { setupTest } from './test-helpers.mjs';
 
 export async function run(config) {
-	const { page } = await setupTest(config, 'alias');
+	const { page } = await setupTest(config, 'hello-world');
 	const content = await page.$eval('body', el => el.textContent);
-	expect(content).toMatch('Aliasing works.');
+	expect(content).toMatch('Hello world');
 }
