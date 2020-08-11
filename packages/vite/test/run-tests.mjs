5142244a8407c0dd2ade4f9f567e707b2a58684a
Marvin Hagemeister <hello@marvinh.dev>
2020-08-11 14:47:32 +0200

    Remove unused path.join

--- packages/vite/test/run-tests.mjs
+++ packages/vite/test/run-tests.mjs
@@ -1,8 +1,7 @@
 import * as pentf from 'pentf';
-import * as path from 'path';
 import { __dirname } from './test-helpers.mjs';
 
 pentf.main({
 	rootDir: __dirname(import.meta.url),
-	testsDir: path.join(__dirname(import.meta.url))
+	testsDir: __dirname(import.meta.url)
 });
