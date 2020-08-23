9c26acead7299a1ff73a5e6ce7a04c184fbbb27a
Jovi De Croock <decroockjovi@gmail.com>
2020-08-23 19:41:23 +0200

    tryout wmr plugin

---
+++ packages/wmr/README.md
@@ -0,0 +1,55 @@
+# Prefresh-vite
+
+[![npm version](https://badgen.net/npm/v/@prefresh/vite)](https://www.npmjs.com/package/@prefresh/vite)
+
+## Setup
+
+```
+npm i -s @prefresh/vite
+## OR
+yarn add @prefresh/vite
+```
+
+Then add it to your `vite.config.js` config:
+
+```js
+import prefresh from '@prefresh/vite';
+
+export default {
+	plugins: [prefresh()]
+};
+```
+
+## Best practices
+
+### Recognition
+
+We need to be able to recognise your components, this means that components should
+start with a capital letter and hook should start with `use` followed by a capital letter.
+This allows the Babel plugin to effectively recognise these.
+
+Do note that a component as seen below is not named.
+
+```jsx
+export default () => {
+  return <p>Want to refresh</p>
+}
+```
+
+Instead do:
+
+```jsx
+const Refresh = () => {
+  return <p>Want to refresh</p>
+}
+
+export default Refresh;
+```
+
+When you are working with HOC's be sure to lift up the `displayName` so we can
+recognise it as a component.
+
+### Context
+
+We don't have a mechanism in place to hot reload context, that's why our general advice is to move
+these to their own files.
---
+++ packages/wmr/src/index.js
@@ -0,0 +1,77 @@
+import MagicString from 'magic-string';
+import { transformSync } from '@babel/core';
+
+const PREFRESH = `
+if (import.meta.hot) {
+  import.meta.hot.accept(async ({module}) => {
+    try {
+      flushUpdates();
+    } catch (e) {
+      import.meta.hot.invalidate();
+    }
+  });
+}
+`;
+
+export default function wmrPlugin() {
+	return {
+		name: 'prefresh-wmr-plugin',
+		transform(code, id) {
+			const ch = id[0];
+			if (
+				process.env.NODE_ENV === 'production' ||
+				ch === '\0' ||
+				ch === '\b' ||
+				!/\.[tj]sx?$/.test(id)
+			)
+				return;
+
+			let hasHot = /(import\.meta\.hot|\$IMPORT_META_HOT\$)/.test(code);
+			let after = '';
+
+			if (
+				code.match(/html`[^`]*<([a-zA-Z][a-zA-Z0-9.:-]*|\$\{.+?\})[^>]*>/) &&
+				code.match(/\bexport\b/)
+			) {
+				hasHot = true;
+				after += '\n' + PREFRESH;
+
+				const result = transform(code, id);
+
+				if (!/\$RefreshReg\$\(/.test(result.code)) {
+					return { code, map: result.map };
+				}
+
+				code = result.code;
+			}
+
+			if (!hasHot) return null;
+
+			const s = new MagicString(code, {
+				filename: id,
+				indentExclusionRanges: undefined
+			});
+
+			s.append(after);
+			s.prepend(
+				`import { createHotContext as $createHotContext$ } from 'wmr';
+        const $IMPORT_META_HOT$ = $createHotContext$(import.meta.url);
+        import '@prefresh/core';
+        import { flushUpdates } from '@prefresh/utils';`
+			);
+
+			return {
+				code: s.toString(),
+				map: s.generateMap({ includeContent: false })
+			};
+		}
+	};
+}
+
+const transform = (code, path) =>
+	transformSync(code, {
+		plugins: [require('react-refresh/babel')],
+		ast: false,
+		sourceMaps: true,
+		sourceFileName: path
+	});
