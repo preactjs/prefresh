a77549711e1d4723d59bf69d1def341c87da831a
jdecroock <decroockjovi@gmail.com>
2020-08-30 16:06:33 +0200

    comment unused func

--- packages/vite/test/index.test.js
+++ packages/vite/test/index.test.js
@@ -32,18 +32,18 @@ async function updateFile(file, replacer) {
 	await fs.writeFile(compPath, replacer(content));
 }
 
-async function expectByPolling(poll, expected) {
-	const maxTries = 20;
-	for (let tries = 0; tries < maxTries; tries++) {
-		const actual = (await poll()) || '';
-		if (actual.indexOf(expected) > -1 || tries === maxTries - 1) {
-			expect(actual).toMatch(expected);
-			break;
-		} else {
-			await timeout(50);
-		}
-	}
-}
+// async function expectByPolling(poll, expected) {
+// 	const maxTries = 20;
+// 	for (let tries = 0; tries < maxTries; tries++) {
+// 		const actual = (await poll()) || '';
+// 		if (actual.indexOf(expected) > -1 || tries === maxTries - 1) {
+// 			expect(actual).toMatch(expected);
+// 			break;
+// 		} else {
+// 			await timeout(50);
+// 		}
+// 	}
+// }
 
 beforeAll(async () => {
 	try {
