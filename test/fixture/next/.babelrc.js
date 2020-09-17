4d4b04f44154f170bbf34e39bd9bae9a10650220
jdecroock <decroockjovi@gmail.com>
2020-09-17 18:02:25 +0200

    fix next

--- test/fixture/next/.babelrc.js
+++ test/fixture/next/.babelrc.js
@@ -1,4 +1,4 @@
 module.exports = {
 	presets: ['next/babel'],
-	plugins: [require.resolve('@prefresh/plugin-babel')]
+	plugins: [[require.resolve('@prefresh/plugin-babel'), { skipEnvCheck: true }]]
 };
