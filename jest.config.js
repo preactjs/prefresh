6740e20944726353c59abd11fe0a6ddd6a89de2a
jdecroock <decroockjovi@gmail.com>
2020-09-17 17:38:59 +0200

    test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
 	testPathIgnorePatterns: ['/node_modules/', '/packages/'],
 	watchPathIgnorePatterns: ['<rootDir>/temp'],
 	moduleNameMapper: {
-		'@prefresh/babel': '<rootDir>/packages/babel',
 		'@prefresh/core': '<rootDir>/packages/core',
 		'@prefresh/utils': '<rootDir>/packages/utils'
 	}
