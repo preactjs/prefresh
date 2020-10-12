48214e9edc0454cd12ad2ec43fa098b5d892409c
Sven Tschui <sventschui@gmail.com>
2020-10-12 22:58:31 +0200

    Use @prefresh/webpack inside @prefresh/next

--- packages/next/src/loader/index.js
+++
@@ -1,7 +0,0 @@
-const { Template } = require('webpack');
-
-const moduleRuntime = Template.getFunctionContent(require('./runtime')).trim();
-
-module.exports = function RefreshHotLoader(source, inputSourceMap) {
-	this.callback(null, source + '\n\n' + moduleRuntime, inputSourceMap);
-};
