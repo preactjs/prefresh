180abc873691824272e301df92255d5d2a0081dd
Jovi De Croock <decroockjovi@gmail.com>
2020-12-05 10:55:25 +0100

    bump fixture deps

--- test/fixture/snowpack/snowpack.config.js
+++ test/fixture/snowpack/snowpack.config.js
@@ -1,16 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-
-const cwd = process.cwd();
-
 module.exports = {
 	mount: {
 		public: '/',
 		src: '/_dist_'
 	},
-	plugins: ['@prefresh/snowpack'],
-	devOptions: {
-		open: 'none',
-		port: 3004
-	}
+	plugins: ['@prefresh/snowpack']
 };
