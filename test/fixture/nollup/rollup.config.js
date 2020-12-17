427f20f32b6e1e429e1b21897aea4f63700f882d
Jovi De Croock <decroockjovi@gmail.com>
2020-12-17 22:43:03 +0100

    pin dep

--- test/fixture/nollup/rollup.config.js
+++ test/fixture/nollup/rollup.config.js
@@ -1,6 +1,5 @@
 import node_resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-import alias from '@rollup/plugin-alias';
 import prefresh from '@prefresh/nollup';
 
 let config = {
@@ -12,11 +11,6 @@ let config = {
 		assetFileNames: '[name].[hash][extname]'
 	},
 	plugins: [
-    alias({
-      entries: {
-        preact: resolve(__dirname, 'node_modules', 'preact'),
-      }
-    }),
 		babel({
 			babelHelpers: 'bundled'
 		}),
