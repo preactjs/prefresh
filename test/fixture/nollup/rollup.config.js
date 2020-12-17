a4aa57d470659bca6e39edc4d985a7a10291aca8
Jovi De Croock <decroockjovi@gmail.com>
2020-12-17 22:27:06 +0100

    add alias for nollup

--- test/fixture/nollup/rollup.config.js
+++ test/fixture/nollup/rollup.config.js
@@ -1,6 +1,6 @@
 import node_resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import alias from '@rollup/plugin-alias';
 import prefresh from '@prefresh/nollup';
 
 let config = {
@@ -12,6 +12,11 @@ let config = {
 		assetFileNames: '[name].[hash][extname]'
 	},
 	plugins: [
+    alias({
+      entries: {
+        preact: resolve(__dirname, 'node_modules', 'preact'),
+      }
+    }),
 		babel({
 			babelHelpers: 'bundled'
 		}),
