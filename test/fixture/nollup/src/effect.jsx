25187e400399158704a29beca8a1c6302f28e6f1
Jovi De Croock <decroockjovi@gmail.com>
2020-12-17 22:51:54 +0100

    fix nollup effect fixture

--- test/fixture/nollup/src/effect.jsx
+++ test/fixture/nollup/src/effect.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'preact/hooks';
 export const Effect = () => {
   const [state, setState] = useState('');
 
-  useEffect(() => { setState('hello w'); }, []);
+  useEffect(() => { setState('hello world'); }, []);
 
   return <p id="effect-test">{state}</p>
 }
