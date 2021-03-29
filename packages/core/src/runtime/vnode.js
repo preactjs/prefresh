73fbc0012701723e5ea98322f8e23edd4c4fd63c
jdecroock <decroockjovi@gmail.com>
2021-03-30 00:24:52 +0200

    cleanup leftover

--- packages/core/src/runtime/vnode.js
+++ packages/core/src/runtime/vnode.js
@@ -15,12 +15,12 @@ options.vnode = vnode => {
   if (vnode && typeof vnode.type === 'function') {
     const vnodes = vnodesForComponent.get(vnode.type);
     if (!vnodes) {
-      vnodesForComponent.set(vnodeType, [vnode]);
+      vnodesForComponent.set(vnode.type, [vnode]);
     } else {
       vnodes.push(vnode);
     }
 
-    const foundType = getMappedVnode(vnodeType);
+    const foundType = getMappedVnode(vnode.type);
     vnode.type = foundType;
     if (vnode[VNODE_COMPONENT]) {
       vnode[VNODE_COMPONENT].constructor = foundType;
