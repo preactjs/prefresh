01218735288c380a1d7ad6909f5b94bff4c77ead
Jovi De Croock <decroockjovi@gmail.com>
2021-04-13 22:34:49 +0200

    (core) - forward thirth prop for catchError (#325)
    * forward thirth prop for catchError
    
    * Create honest-pets-raise.md

---
+++ .changeset/honest-pets-raise.md
@@ -0,0 +1,5 @@
+---
+"@prefresh/core": patch
+---
+
+Forward thirth prop for catchError
--- packages/core/src/runtime/catchError.js
+++ packages/core/src/runtime/catchError.js
@@ -6,10 +6,10 @@ import {
 } from '../constants';
 
 const oldCatchError = options[CATCH_ERROR_OPTION];
-options[CATCH_ERROR_OPTION] = (error, vnode) => {
+options[CATCH_ERROR_OPTION] = (error, vnode, oldVNode) => {
   if (vnode[VNODE_COMPONENT] && vnode[VNODE_COMPONENT][COMPONENT_DIRTY]) {
     vnode[VNODE_COMPONENT][COMPONENT_DIRTY] = false;
   }
 
-  if (oldCatchError) oldCatchError(error, vnode);
+  if (oldCatchError) oldCatchError(error, vnode, oldVNode);
 };
