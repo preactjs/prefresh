34ff2b48c9a218fb75e6511e0204ab4a81b23a15
Bird <42877339+dbr031@users.noreply.github.com>
2021-05-12 14:17:15 +0200

    Upgrade & simplify webpack example (#343)
    * (webpack) - upgrade webpack example packages
    
    * (webpack) - simplify webpack example config
    
    * (webpack) - simplify webpack example counter

--- packages/webpack/example/.babelrc.js
+++ packages/webpack/example/.babelrc.js
@@ -2,12 +2,9 @@ module.exports = {
   presets: ['@babel/preset-env'],
   plugins: [
     '@prefresh/babel-plugin',
-    [
-      '@babel/plugin-transform-react-jsx',
-      {
-        pragma: 'h',
-        pragmaFrag: 'Fragment',
-      },
-    ],
+    ['@babel/plugin-transform-react-jsx', {
+      pragma: 'h',
+      pragmaFrag: 'Fragment',
+    }],
   ],
 };
