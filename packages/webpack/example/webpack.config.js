6cf697ac5acd37a1d080ad02cc777906d1a798ab
Bird <42877339+dbr031@users.noreply.github.com>
2021-05-12 12:09:33 +0200

    (webpack) - simplify webpack example config

--- packages/webpack/example/webpack.config.js
+++ packages/webpack/example/webpack.config.js
@@ -1,62 +1,33 @@
-const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const PreactRefreshPlugin = require('@prefresh/webpack');
 
-const makeConfig = () => {
-  const { NODE_ENV } = process.env;
+module.exports = (env) => {
+  const { NODE_ENV = 'development' } = env;
   const isProduction = NODE_ENV === 'production';
 
-  // Build plugins
   const plugins = [
     new HtmlWebpackPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-    }),
   ];
-
   if (!isProduction) {
-    plugins.push(new PreactRefreshPlugin());
     plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new PreactRefreshPlugin());
   }
 
-  // Return configuration
   return {
     mode: isProduction ? 'production' : 'development',
     entry: './src/index.js',
-    stats: 'normal',
     devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      host: 'localhost',
-      port: 3000,
-      historyApiFallback: true,
       hot: true,
-      inline: true,
-      publicPath: '/',
-      clientLogLevel: 'none',
-      open: true,
-      overlay: true,
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-    },
-    resolve: {
-      alias: {
-        preact: path.resolve(__dirname, 'node_modules', 'preact'),
-      },
     },
     plugins,
     module: {
       rules: [
         {
-          test: /\.js$|\.jsx$/,
-          include: [path.resolve(__dirname, 'src')],
+          test: /\.jsx?$/,
           loader: 'babel-loader',
         },
       ],
     },
   };
 };
-
-module.exports = makeConfig();
