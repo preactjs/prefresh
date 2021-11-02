718d21209365bdf298e858d8792b1334c442840a
Tiger Oakes <contact@tigeroakes.com>
2021-11-02 09:53:23 -0700

    Include all options in vite typings (#395)
    * Include all options in typings
    
    * Create cyan-doors-relax.md
    
    Co-authored-by: Jovi De Croock <decroockjovi@gmail.com>

--- packages/vite/index.d.ts
+++ packages/vite/index.d.ts
@@ -1,7 +1,10 @@
+import { FilterPattern } from '@rollup/pluginutils';
 import { Plugin } from 'vite';
 
 export interface Options {
-  parserPlugins?: string[];
+  parserPlugins?: readonly string[];
+  include?: FilterPattern;
+  exclude?: FilterPattern;
 }
 
 declare const prefreshPlugin: (options?: Options) => Plugin;
