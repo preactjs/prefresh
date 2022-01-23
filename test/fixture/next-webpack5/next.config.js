1b3c19cf6e2667738e39450cd6c87bf2af8c979a
JoviDeCroock <decroockjovi@gmail.com>
2022-01-23 19:13:24 +0100

    fixes

--- test/fixture/next-webpack5/next.config.js
+++ test/fixture/next-webpack5/next.config.js
@@ -2,49 +2,23 @@ const withPrefresh = require('@prefresh/next');
 const path = require('path');
 
 const config = {
-	experimental: {
-		modern: true,
-    polyfillsOptimization: true,
-  },
-  future: {
-    webpack5: true,
-  },
-	webpack(config, { dev, isServer }) {
-		const splitChunks = config.optimization && config.optimization.splitChunks;
-		if (splitChunks && isServer && splitChunks.cacheGroups) {
-			const cacheGroups = splitChunks.cacheGroups;
-			const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
-
-			if (cacheGroups.framework) {
-				cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-					test: preactModules
-				});
-				cacheGroups.commons.name = 'framework';
-			} else {
-				cacheGroups.preact = {
-					name: 'commons',
-					chunks: 'all',
-					test: preactModules
-				};
-			}
-		}
+	webpack(config) {
+    const splitChunks =
+      config.optimization && config.optimization.splitChunks;
+    if (splitChunks && splitChunks.cacheGroups) {
+      const cacheGroups = splitChunks.cacheGroups;
+      const test = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+      if (cacheGroups.framework) {
+        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+          test
+        });
+      }
+    }
 
 		// Install webpack aliases:
 		const aliases = config.resolve.alias || (config.resolve.alias = {});
 		aliases.react = aliases['react-dom'] = 'preact/compat';
 		aliases.preact = path.resolve(__dirname, 'node_modules', 'preact');
-
-		// inject Preact DevTools
-		if (dev && !isServer) {
-			const entry = config.entry;
-			config.entry = () =>
-				entry().then(entries => {
-					entries['main.js'] = ['preact/debug'].concat(
-						entries['main.js'] || []
-					);
-					return entries;
-				});
-		}
 
 		return config;
 	}
