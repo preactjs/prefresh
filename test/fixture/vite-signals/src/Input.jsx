6c3788ea0fc73743310d6767997b66a4d7af028a
Jovi De Croock <decroockjovi@gmail.com>
2022-09-08 09:49:10 +0200

    fixies

--- test/fixture/vite-signals/src/Input.jsx
+++ test/fixture/vite-signals/src/Input.jsx
@@ -4,7 +4,6 @@ import { signal } from '@preact/signals';
 const inputValue = signal('foo')
 export function Input() {
   return (
-    // TODO: after merging https://github.com/preactjs/signals/pull/76 make this value={signal}
     <input className='input' value={inputValue} onInput={e => { inputValue.value = e.currentTarget.value }} />
   )
 }
